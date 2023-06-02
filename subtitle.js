a40fe4d4505cf58a157ff659f9ce8cd7a59fc7c5
Sun Fi <jytrhraegdrf@protonmail.com>
2023-06-02 06:22:04 +0300

    new subtitles

--- subtitle.js
+++ subtitle.js
@@ -1,5 +1,24 @@
 function getSubtitle(){
-    const messages = ['the duckening', 'frogs', 'i forgor 💀', 'space pirates', 'E', 'object Object', '|  ||  ||  | --', 'a̵̧̧̛̦̳̠͙̟̗̺͍͎̼͍͈͓̤̝͛̅̄̈̓́͘͠͝a̶̖̻̬̲̭̯̺̫̪̠̱̞͂̔̆̊̓͘͝ͅạ̷̡͚͕̤͙̜̜̔̓́͑͛͛̃̕ä̷̢̛̜̤̖̞̗̰͓̣͔̜͉́͌á̷̡̨̰͕̟̟͎͙̠̖̬̮́̐̌̈́̃̅̈́̉͒̏́̇͊̚͝͝ͅa̵͔̗̭͕̮̩͇̺͈̤̍̃̔͐̽͆̏̇̽̀͘̚̚̕͠͠ͅa̸̢̛͔͉̭͓̬͖̼̺̟͔͉͛̄̈̈̑̈́͋̀̆̐̉̇̀͌̇͠a̷̡͙̘̜͈̥̬̫̺̿̔͘ͅa̵̢̧͍̺̩̻͔̳͕̞̗͙͓̮͌̎̍̀̀̊̍̿̂̈́̃́͆͌̄̚̕͜a̴̞͉̺̤̍̓͒͗̽̐͒̐͜͠', 'can probably run in Internet Explorer'];
+    const messages = [
+        'the duckening',
+        'frogs',
+        'i forgor 💀',
+        'space pirates',
+        'E',
+        'object Object',
+        '|  ||  ||  | --',
+        'a̵̧̧̛̦̳̠͙̟̗̺͍͎̼͍͈͓̤̝͛̅̄̈̓́͘͠͝a̶̖̻̬̲̭̯̺̫̪̠̱̞͂̔̆̊̓͘͝ͅạ̷̡͚͕̤͙̜̜̔̓́͑͛͛̃̕ä̷̢̛̜̤̖̞̗̰͓̣͔̜͉́͌á̷̡̨̰͕̟̟͎͙̠̖̬̮́̐̌̈́̃̅̈́̉͒̏́̇͊̚͝͝ͅa̵͔̗̭͕̮̩͇̺͈̤̍̃̔͐̽͆̏̇̽̀͘̚̚̕͠͠ͅa̸̢̛͔͉̭͓̬͖̼̺̟͔͉͛̄̈̈̑̈́͋̀̆̐̉̇̀͌̇͠a̷̡͙̘̜͈̥̬̫̺̿̔͘ͅa̵̢̧͍̺̩̻͔̳͕̞̗͙͓̮͌̎̍̀̀̊̍̿̂̈́̃́͆͌̄̚̕͜a̴̞͉̺̤̍̓͒͗̽̐͒̐͜͠',
+        'can probably run in Internet Explorer',
+        'Blood for the Blood God',
+        'quack for your life',
+        'greetings traveller',
+        'i made this instead of studying for exam',
+        'consider sleeping',
+        'smol duck',
+        'no regrets',
+        'highly illegal',
+        'use DuckDuckGo'
+        ];
     const randomIndex = Math.round(Math.random()*messages.length);
     document.getElementById("subtitle").innerHTML = messages[randomIndex];
 }
